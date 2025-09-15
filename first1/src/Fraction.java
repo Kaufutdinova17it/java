e086c4cccf707f7b173c09a847b715ed98f0db03
Kaufutdinova17it <kap3451@outlook.com>
2025-09-15 23:44:17 +0500

    Add files via upload
    Добавила первую лабу по java

---
+++ first1/src/Fraction.java
@@ -0,0 +1,243 @@
+public class Fraction
+{
+    public static double fraction(double x) {
+        int wholePart = (int) x;
+        return x - wholePart;
+    }
+    public static int charToNum(char x){
+        if (x>= '0' && x <= '9') {
+            return x - '0';
+        }
+        else {
+            System.out.println("Ошибка: символ не является цифрой");
+            return -1;
+        }
+
+    }
+    public static boolean is2Digits(int x) {
+        return (x >= 10 && x <= 99) || (x <= -10 && x >= -99);
+    }
+    public static boolean isInRange (int a,int b,int num) {
+        return (a <= b && num >= a && num <= b) || (a >= b && num <= a && num >= b);
+    }
+    public static boolean isEqual(int a,int b,int c){
+        return a == b && b == c;
+    }
+    public static int abc(int x) {
+        if (x >= 0){
+            return x;
+        }
+        else {
+            return -x;
+        }
+    }
+    public static boolean is35(int x){
+        return (x % 3 == 0 || x % 5 == 0) && !(x % 3 == 0 && x % 5 == 0);
+    }
+
+    public static int max3(int x,int y,int z) {
+        int max = x;
+        if (y > max){
+            max = y;
+        }
+        if (z > max){
+            max = z;
+        }
+        return max;
+
+    }
+    public static int sum2(int x,int y) {
+        int z = x + y;
+        if (z >= 10 && z <= 19 ) {
+            return 20;
+        }
+        else {
+            return z;
+        }
+    }
+    public static String day(int x) {
+        switch (x) {
+            case 1:
+                System.out.println("Понедельник");
+                break;
+            case 2:
+                System.out.println("Вторник");
+                break;
+            case 3:
+                System.out.println("Среда");
+                break;
+            case 4:
+                System.out.println("Четверг");
+                break;
+            case 5:
+                System.out.println("Пятница");
+                break;
+            case 6:
+                System.out.println("Суббота");
+                break;
+            case 7:
+                System.out.println("Воскресенье");
+                break;
+            default:
+                System.out.println("Это не день недели");
+                break;
+        }
+
+        return "";
+    }
+    public static String listNums(int x) {
+        String result = "";
+        int count = 0;
+
+        while (count <= x) {
+            result = result + count + " ";
+            count = count + 1;
+        }
+
+        return result;
+    }
+    public static String chet(int x) {
+        String result = "";
+        int count = 0;
+
+        while (count <= x) {
+            result = result + count + " ";
+            count = count + 2;
+        }
+
+        return result;
+    }
+    public static int numLen(long x) {
+        if (x == 0) {
+            return 1;
+        }
+
+        // Используем математику для избежания переполнения
+        int count = 0;
+        long num = x;
+
+        // Обрабатываем отрицательные числа без изменения знака
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+
+        // Если число было отрицательным, считаем знак "-" как отдельный символ?
+        // Но по условию задачи нужно считать только цифры
+        return count;
+    }
+    public static void square(int x){
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+    public static void rightTriangle(int x){
+        for (int i = 1; i <= x; i++) {
+            for (int j = 0; j < x - i; j++) {
+                System.out.print(' ');
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+    public static int findFirst(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int maxAbs(int[] arr) {
+        int maxAbsValue = arr[0]; // Начинаем с первого элемента
+        int maxAbs = Math.abs(arr[0]); // Модуль первого элемента
+
+        for (int i = 1; i < arr.length; i++) {
+            int currentAbs = Math.abs(arr[i]);
+            if (currentAbs > maxAbs) {
+                maxAbs = currentAbs;
+                maxAbsValue = arr[i]; // Сохраняем само число, а не его модуль
+            }
+        }
+
+        return maxAbsValue;
+    }
+    public static int[] add(int[] arr, int[] ins, int pos) {
+        // Обработка null
+        if (arr == null) arr = new int[0];
+        if (ins == null) ins = new int[0];
+
+        // Корректировка позиции
+        if (pos < 0) pos = 0;
+        if (pos > arr.length) pos = arr.length;
+
+        int[] result = new int[arr.length + ins.length];
+
+        // Копируем первую часть
+        for (int i = 0; i < pos; i++) {
+            result[i] = arr[i];
+        }
+
+        // Вставляем новый массив
+        for (int i = 0; i < ins.length; i++) {
+            result[pos + i] = ins[i];
+        }
+
+        // Копируем оставшуюся часть
+        for (int i = pos; i < arr.length; i++) {
+            result[ins.length + i] = arr[i];
+        }
+
+        return result;
+    }
+    public static int[] reverseBack(int[] arr) {
+        if (arr == null) {
+            return new int[0];
+        }
+
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = arr[arr.length - 1 - i];
+        }
+
+        return result;
+    }
+    public static int[] findAll(int[] arr, int x) {
+        if (arr == null) {
+            return new int[0];
+        }
+
+        // 1. Подсчитываем количество вхождений
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                count++;
+            }
+        }
+
+
+        int[] result = new int[count];
+        int resultIndex = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                result[resultIndex] = i;
+                resultIndex++;
+            }
+        }
+
+        return result;
+    }
+
+
+
+
+}
+
---
+++ first1/src/Main.java
@@ -0,0 +1,358 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number1;
+        int number2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите номер блока задач (1-4)");
+        number1 = scanner.nextInt();
+
+        switch (number1) {
+            case 1:
+                System.out.println("Выберите номер задачи (нечетный:1,3,5,7,9)");
+                number2 = scanner.nextInt();
+                switch (number2) {
+                    case 1:
+                        task1_1(scanner);
+                        break;
+                    case 3:
+                        task1_3(scanner);
+                        break;
+                    case 5:
+                        task1_5(scanner);
+                        break;
+                    case 7:
+                        task1_7(scanner);
+                        break;
+                    case 9:
+                        task1_9(scanner);
+                        break;
+                    default:
+                        System.out.println("Ошибка!Неверный номер задачи!");
+                        break;
+                }
+                break;
+
+            case 2:
+                System.out.println("Выберите номер задачи (нечетный:1,3,5,7,9)");
+                number2 = scanner.nextInt();
+                switch (number2) {
+                    case 1:
+                        task2_1(scanner);
+                        break;
+                    case 3:
+                        task2_3(scanner);
+                        break;
+                    case 5:
+                        task2_5(scanner);
+                        break;
+                    case 7:
+                        task2_7(scanner);
+                        break;
+                    case 9:
+                        task2_9(scanner);
+                        break;
+                    default:
+                        System.out.println("Ошибка!Неверный номер задачи!");
+                        break;
+                }
+                break;
+
+            case 3:
+                System.out.println("Выберите номер задачи (нечетный:1,3,5,7,9)");
+                number2 = scanner.nextInt();
+                switch (number2) {
+                    case 1:
+                        task3_1(scanner);
+                        break;
+                    case 3:
+                        task3_3(scanner);
+                        break;
+                    case 5:
+                        task3_5(scanner);
+                        break;
+                    case 7:
+                        task3_7(scanner);
+                        break;
+                    case 9:
+                        task3_9(scanner);
+                        break;
+                    default:
+                        System.out.println("Ошибка!Неверный номер задачи!");
+                        break;
+                }
+                break;
+
+            case 4:
+                System.out.println("Выберите номер задачи (нечетный:1,3,5,7,9)");
+                number2 = scanner.nextInt();
+                switch (number2) {
+                    case 1:
+                        task4_1(scanner);
+                        break;
+                    case 3:
+                        task4_3(scanner);
+                        break;
+                    case 5:
+                        task4_5(scanner);
+                        break;
+                    case 7:
+                        task4_7(scanner);
+                        break;
+                    case 9:
+                        task4_9(scanner);
+                        break;
+                    default:
+                        System.out.println("Ошибка!Неверный номер задачи!");
+                        break;
+                }
+                break;
+
+            default:
+                System.out.println("Ошибка!Неверный номер блока!");
+                break;
+        }
+        scanner.close();
+    }
+
+
+    public static void task1_1(Scanner scanner) {
+        System.out.print("Введите вещественное число: ");
+        double number = scanner.nextDouble();
+        double result1_1 = Fraction.fraction(number);
+        System.out.println("Дробная часть: " + result1_1);
+    }
+
+    public static void task1_3(Scanner scanner) {
+        System.out.print("Введите цифру (0-9): ");
+        char symbol = scanner.next().charAt(0);
+        int result1_3 = Fraction.charToNum(symbol);
+        System.out.println("Число: " + result1_3);
+    }
+
+    public static void task1_5(Scanner scanner) {
+        System.out.println("Введите целое число:");
+        int number1_5 = scanner.nextInt();
+        boolean result1_5 = Fraction.is2Digits(number1_5);
+        System.out.println(result1_5);
+    }
+
+    public static void task1_7(Scanner scanner) {
+        System.out.println("Введите первую целую границу");
+        int a = scanner.nextInt();
+        System.out.println("Введите вторую целую границу");
+        int b = scanner.nextInt();
+        System.out.println("Введите целое число,которое хотите проверить");
+        int num = scanner.nextInt();
+        boolean result1_7 = Fraction.isInRange(a,b,num);
+        System.out.println(result1_7);
+
+    }
+
+    public static void task1_9(Scanner scanner) {
+        System.out.println("Введите целое число a");
+        int a = scanner.nextInt();
+        System.out.println("Введите целое число b");
+        int b = scanner.nextInt();
+        System.out.println("Введите целое число c");
+        int c = scanner.nextInt();
+        boolean result1_9 = Fraction.isEqual(a,b,c);
+        System.out.println(result1_9);
+    }
+
+
+    public static void task2_1(Scanner scanner) {
+        System.out.println("Введите целое число:");
+        int x = scanner.nextInt();
+        int result2_1 = Fraction.abc(x);
+        System.out.println("Модуль числа:" + result2_1);
+    }
+
+    public static void task2_3(Scanner scanner) {
+        System.out.println("Введите целое число:");
+        int x = scanner.nextInt();
+        boolean result2_3 = Fraction.is35(x);
+        System.out.println(result2_3);
+
+    }
+
+    public static void task2_5(Scanner scanner) {
+        System.out.println("Введите целое число x");
+        int x = scanner.nextInt();
+        System.out.println("Введите целое число y");
+        int y = scanner.nextInt();
+        System.out.println("Введите целое число z");
+        int z = scanner.nextInt();
+        int result2_5 = Fraction.max3(x,y,z);
+        System.out.println("Максимальное из трех чисел:"+result2_5);
+    }
+
+    public static void task2_7(Scanner scanner) {
+        System.out.println("Введите первое целое число");
+        int x = scanner.nextInt();
+        System.out.println("Введите второе целое число");
+        int y = scanner.nextInt();
+        int result2_7 = Fraction.sum2(x,y);
+        System.out.println("Вычесленное значение:" + result2_7);
+    }
+
+    public static <string> void task2_9(Scanner scanner) {
+        System.out.println("Введите целое число от 1 до 7");
+        int x = scanner.nextInt();
+        string result2_9 = (string) Fraction.day(x);
+
+
+    }
+
+
+    public static void task3_1(Scanner scanner) {
+        System.out.println("Введите целое число");
+        int x = scanner.nextInt();
+        String result3_1;
+        result3_1 = Fraction.listNums(x);
+        System.out.println(result3_1);
+    }
+
+    public static void task3_3(Scanner scanner) {
+        System.out.println("Введите целое число");
+        int x = scanner.nextInt();
+        String result3_3;
+        result3_3 = Fraction.chet(x);
+        System.out.println(result3_3);
+
+    }
+
+    public static void task3_5(Scanner scanner) {
+        System.out.println("Введите целое положительное число");
+        int x = scanner.nextInt();
+        int result3_5 = Fraction.numLen(x);
+        System.out.println(result3_5);
+
+    }
+
+    public static void task3_7(Scanner scanner) {
+        System.out.println("Введите целое число");
+        int x = scanner.nextInt();
+        Fraction.square(x);
+    }
+
+    public static void task3_9(Scanner scanner) {
+        System.out.println("Введите целое число");
+        int x = scanner.nextInt();
+        Fraction.rightTriangle(x);
+    }
+
+
+    public static void task4_1(Scanner scanner) {
+        System.out.println("Введите длину массива: ");
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        System.out.println("Введите элементы массива: ");
+        for (int i = 0; i < length;i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Введите число для поиска: ");
+        int x = scanner.nextInt();
+        int result4_1 = Fraction.findFirst(arr,x);
+        System.out.println("Индекс первого вхождения: " + result4_1);
+    }
+
+    public static void task4_3(Scanner scanner) {
+        System.out.println("Введите длину массива: ");
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        System.out.println("Введите элементы массива: ");
+        for (int i = 0; i < length;i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int result4_3 = Fraction.maxAbs(arr);
+        System.out.println("Модуль наибольшего числа: " + result4_3);
+    }
+
+
+    public static void task4_5(Scanner scanner) {
+        // Ввод первого массива arr
+        System.out.println("Введите длину первого массива: ");
+        int lengthArr = scanner.nextInt();
+        int[] arr = new int[lengthArr];
+        System.out.println("Введите элементы первого массива: ");
+        for (int i = 0; i < lengthArr; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Ввод второго массива ins
+        System.out.println("Введите длину второго массива: ");
+        int lengthIns = scanner.nextInt();
+        int[] ins = new int[lengthIns];
+        System.out.println("Введите элементы второго массива: ");
+        for (int i = 0; i < lengthIns; i++) {
+            ins[i] = scanner.nextInt();
+        }
+
+        // Ввод позиции для вставки
+        System.out.println("Введите позицию для вставки: ");
+        int pos = scanner.nextInt();
+
+        // Вызов метода add
+        int[] result = Fraction.add(arr, ins, pos);
+        System.out.print("Результат: [");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i < result.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public static void task4_7(Scanner scanner) {
+        System.out.println("Введите длину массива: ");
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        System.out.println("Введите элементы массива: ");
+        for (int i = 0; i < length;i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int[] reversedArray = Fraction.reverseBack(arr);
+        System.out.print("Новый массив: [");
+        for (int i = 0; i < reversedArray.length; i++) {
+            System.out.print(reversedArray[i]);
+            if (i < reversedArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public static void task4_9(Scanner scanner) {
+        System.out.println("Введите длину массива: ");
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        System.out.println("Введите элементы массива: ");
+
+        // 1. Сначала заполняем весь массив
+        for (int i = 0; i < length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // 2. Только после заполнения всего массива запрашиваем число для поиска
+        System.out.println("Введите число для поиска: ");
+        int x = scanner.nextInt();
+
+        // 3. Вызываем метод поиска
+        int[] result4_9 = Fraction.findAll(arr, x);
+
+        // 4. Выводим результат
+        System.out.print("Индексы вхождений: [");
+        for (int c = 0; c < result4_9.length; c++) {
+            System.out.print(result4_9[c]);
+            if (c < result4_9.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+        }
+    }
